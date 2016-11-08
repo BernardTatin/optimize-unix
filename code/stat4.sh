f6040a4f6e4fde1a90729dff740e3fdc785cdee3
Tatin Bernard <bernard.Tatin@ac-orleans-tours.fr>
2016-11-08 12:07:32 +0100

    That's right, folks

--- code/stat4.sh
+++ code/stat4.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 scriptname="$(basename $0)"
-set -e
+# set -e
 
 dohelp() {
     cat << DOHELP
@@ -38,27 +38,26 @@ set_byhour() {
 
 doit() {
     end=1
-    cmd="cat $@ | ${byhour} | sort | uniq -c | sort -nr | head -n ${size} ${after}"
+    cmd="cat $@ | LC_ALL=C tr -cd '\t\n[:print:]' | ${byhour} | sort | uniq -c | sort -nr | head -n ${size} ${after}"
     case ${is_byhour} in
         0)
             printf "%-7.7s %-6.6s %-2.2s\n" "occurences" "date" "hour"
             printf "%-17.17s\n" "-----------------------------------"
+			eval "$cmd"
             ;;
         1)
             printf "%-7.7s %-2.2s\n" "occurences" "hour"
             printf "%-10.10s\n" "-----------------------------------"
+			while read count index reste; do
+				local h=${index#0}
+				hours[h]=${count}
+			done <<<"$(eval ${cmd})"
+			for ((i=0; i<24; i++)) 
+			do
+				printf "%7d %02d\n" ${hours[i]} $i
+			done
             ;;
     esac
-	# eval "$cmd"
-	while read count index reste; do
-		local h=${index#0}
-		# printf "<%2d> <%7d> <%s>\n" $h $count $reste
-		hours[h]=${count}
-	done <<<"$(eval ${cmd})"
-	for ((i=0; i<24; i++)) 
-	do
-		printf "%7d %02d\n" ${hours[i]} $i
-	done
 }
 
 [ $# -eq 0 ] && dohelp
